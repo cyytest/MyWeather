e023716fb6f9f9a62aea730b149b88ef6ccd9723
cyytest <642575685@qq.com>
2015-03-20 16:05:52 +0800

    1.0

--- app/src/main/java/pku/ss/cyy/myweather/MainActivity.java
+++ app/src/main/java/pku/ss/cyy/myweather/MainActivity.java
@@ -1,17 +1,39 @@
 package pku.ss.cyy.myweather;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.zip.GZIPInputStream;
+
+import pku.ss.cyy.util.NetUtil;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+    private ImageView mUpdateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        mUpdateBtn = (ImageView)findViewById(R.id.title_update_btn);
+        mUpdateBtn.setOnClickListener(this);
     }
 
 
@@ -35,5 +57,54 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.title_update_btn) {
+            SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
+            String cityCode = sharedPreferences.getString("main_city_code","101010100");
+            Log.d("myWeather",cityCode);
+            if(NetUtil.getNetworkState(this) != NetUtil.NETWORK_NONE) {
+                Log.d("MyWeather", "网络正常！");
+                queryWeatherCode(cityCode);
+            }
+            else {
+                Log.d("MyWeather", "网络错误！");
+                Toast.makeText(getApplicationContext(),"网络未连接！",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void queryWeatherCode(String cityCode) {
+        final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey=" + cityCode;
+        Log.d("MyWeather", address);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpGet httpGet = new HttpGet(address);
+                    HttpResponse httpResponse = httpClient.execute(httpGet);
+                    if(httpResponse.getStatusLine().getStatusCode() == 200) {
+                        HttpEntity entity = httpResponse.getEntity();
+
+                        InputStream inputStream = entity.getContent();
+                        inputStream = new GZIPInputStream(inputStream);
+
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                        StringBuilder response = new StringBuilder();
+                        String str;
+                        while ((str=reader.readLine()) != null) {
+                            response.append(str);
+                        }
+                        String responseStr = response.toString();
+                        Log.d("myWeather", responseStr);
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
